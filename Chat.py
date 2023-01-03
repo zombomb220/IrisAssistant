e08acb6f4d343f95d8d84c4c320ce8ee4de00913
Jordan <zombomb220@gmail.com>
2023-01-02 21:43:00 -0800

    removed leaked (and now retired) API key, moved to environ based key management.

--- Chat.py
+++ Chat.py
@@ -1,8 +1,9 @@
 import openai
+import os
 
 
 def chat(prompt) -> str:
-    openai.api_key = "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"
+    openai.api_key = os.environ['OPENAI_API_KEY']
     completions = openai.Completion.create(
         engine="text-davinci-003",
         prompt=prompt,
@@ -15,12 +16,4 @@ def chat(prompt) -> str:
     message = completions.choices[0].text
     print("Response: " + message)
     return message.strip()
-
-
-if __name__ == '__main__':
-    openai.api_key = "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"
-    while True:
-        prompt = input("Enter a message: ")
-        response = chat(prompt)
-        print(response)
 
